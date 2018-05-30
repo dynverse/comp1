d16325ce186e352265373549e99db7da72ede78b
zouter <wouter.saelens@gmail.com>
2018-05-30 17:17:25 +0200

    add some comments

--- run.R
+++ run.R
@@ -1,12 +1,17 @@
 library(tidyverse)
 
+## Load data -----------------------------------------------
+
 expression <- read.csv("/input/expression.csv", row.names=1, header = TRUE) %>%
   as.matrix()
 params <- jsonlite::read_json("/input/params.json", simplifyVector = TRUE)
 start_cells <- jsonlite::read_json("/input/start_cells.json", simplifyVector = TRUE)
 
+## Trajectory inference -----------------------------------
+# do PCA
 pca <- prcomp(expression)
 
+# extract the component and use it as pseudotimes
 pseudotimes <- pca$x[, params$component]
 
 # flip pseudotimes using start_cells
@@ -16,6 +21,8 @@ if (!is.null(start_cells)) {
   }
 }
 
+## Save output ---------------------------------------------
+# output pseudotimes
 tibble(
   cell_id = names(pseudotimes),
   pseudotime = pseudotimes
