2c39dce324a5502fbf68780be620f513a62eaedb
zouter <wouter.saelens@gmail.com>
2018-06-07 10:52:34 +0200

    update build all

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,6 @@
-docker build python_hdf5 -t dynverse/comp1:python_hdf5
-docker build python_text -t dynverse/comp1:python_text
-docker build R_text -t dynverse/comp1:R_text
-docker build R_hdf5 -t dynverse/comp1:R_hdf5
-docker build R_dynwrap -t dynverse/comp1:R_dynwrap
-docker build R_rds -t dynverse/comp1:R_rds
-docker build R_dynwrap -t dynverse/comp1:latest
+for tag in */ ; do
+  echo $tag
+  docker build ${tag::-1} -t dynverse/comp1:${tag::-1}
+done
 
 docker push dynverse/comp1
