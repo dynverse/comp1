eeff68074116e80fbf10ca350b741abb1b8aea6e
Robrecht Cannoodt <rcannood@gmail.com>
2018-10-29 19:38:32 +0100

    remove feather as per dynwrap 0.2.0

--- build_all.R
+++ build_all.R
@@ -18,11 +18,9 @@ for (tag in tags) {
 
 # system("docker push dynverse/dynwrap_tester")
 
-# sudo singularity build python_feather.simg python_feather/Singularity.python_feather
 # sudo singularity build python_hdf5.simg python_hdf5/Singularity.python_hdf5
 # sudo singularity build python_text.simg python_text/Singularity.python_text
 # sudo singularity build R_dynwrap.simg R_dynwrap/Singularity.R_dynwrap
-# sudo singularity build R_feather.simg R_feather/Singularity.R_feather
 # sudo singularity build R_hdf5.simg R_hdf5/Singularity.R_hdf5
 # sudo singularity build R_rds.simg R_rds/Singularity.R_rds
 # sudo singularity build R_text.simg R_text/Singularity.R_text
